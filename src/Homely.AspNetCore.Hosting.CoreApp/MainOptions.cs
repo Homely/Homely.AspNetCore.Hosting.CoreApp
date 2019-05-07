1b701136943c308709eb14735d64c89dfb235735
Pure Krome <pure.krome@world-domination.com.au>
2019-05-07 22:26:03 +1000

    Better logging checks and some starting options.

---
+++ src/Homely.AspNetCore.Hosting.CoreApp/MainOptions.cs
@@ -0,0 +1,27 @@
+﻿namespace Homely.AspNetCore.Hosting.CoreApp
+{
+    public class MainOptions
+    {
+        /// <summary>
+        /// Command line arguments.
+        /// </summary>
+        public string[] CommandLineArguments { get; set; }
+
+        /// <summary>
+        /// Optional text which is first displayed when the application starts. This can be useful to help determine if things have started and are working ok.
+        /// </summary>
+        public string FirstLoggingInformationMessage { get; set; }
+
+        /// <summary>
+        /// The name of the Environment Variable which contains the 'Environment' value (e.g. Development, Production, etc).
+        /// </summary>
+        /// <remarks>Defaults to <code>ASPNETCORE_ENVIRONMENT</code></remarks>
+        public string EnvironmentVariableKey { get; set; } = "ASPNETCORE_ENVIRONMENT";
+
+        /// <summary>
+        /// Is the main application.json file, optional?
+        /// </summary>
+        /// <remarks>Defaults to <code>true</code>.</remarks>
+        public bool ApplicationJsonFileOptional { get; set; } = true;
+    }
+}
--- src/Homely.AspNetCore.Hosting.CoreApp/Program.cs
+++ src/Homely.AspNetCore.Hosting.CoreApp/Program.cs
@@ -10,41 +10,104 @@ namespace Homely.AspNetCore.Hosting.CoreApp
 {
     public static class Program
     {
+        /// <summary>
+        /// The program's main start/entry point. Hold on to your butts .... here we go!
+        /// </summary>
+        /// <typeparam name="T">Startup class type.</typeparam>
+        /// <param name="args">Optional command line arguments.</param>
+        /// <returns></returns>
         public static async Task Main<T>(string[] args) where T : class
         {
-            Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(Configuration)
+            var options = new MainOptions
+            {
+                CommandLineArguments = args
+            };
+
+            await Program.Main<T>(options);
+        }
+
+        /// <summary>
+        /// The program's main start/entry point. Hold on to your butts .... here we go!
+        /// </summary>
+        /// <typeparam name="T">Startup class type.</typeparam>
+        /// <param name="options">Options to help setup/configure your program.</param>
+        /// <returns>Task of this Main application run.</returns>
+        public static async Task Main<T>(MainOptions options) where T : class
+        {
+            try
+            {
+                if (options == null)
+                {
+                    throw new ArgumentNullException(nameof(options));
+                }
+
+                Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(GetConfigurationBuilder(options.EnvironmentVariableKey))
                 .Enrich.FromLogContext()
                 .CreateLogger();
 
-            try
-            {
-                await CreateWebHostBuilder<T>(args).Build()
-                                                   .RunAsync();
+                if (!string.IsNullOrWhiteSpace(options.FirstLoggingInformationMessage))
+                {
+                    Log.Information(options.FirstLoggingInformationMessage);
+                }
+
+                await CreateWebHostBuilder<T>(options.CommandLineArguments).Build()
+                                                           .RunAsync();
             }
             catch (Exception exception)
             {
-                // TODO: Add metrics (like Application Insights?) to log telemetry failures.
-                Log.Logger.Fatal(exception, "Host terminated unexpectantly. Sadness :~(");
+                const string errorMessage = "Host terminated unexpectantly. Sadness :~(";
+                if (Log.Logger == null)
+                {
+                    Console.WriteLine(errorMessage);
+                    Console.WriteLine(exception.Message);
+                }
+                else
+                {
+                    // TODO: Add metrics (like Application Insights?) to log telemetry failures.
+                    Log.Logger.Fatal(exception, errorMessage);
+                }
             }
             finally
             {
-                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
-                Log.CloseAndFlush();
+                const string shutdownMessage = "Application has now shutdown.";
+
+                if (Log.Logger != null)
+                {
+                    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                    Log.CloseAndFlush();
+
+                    Log.Information(shutdownMessage);
+                }
+                else
+                {
+                    Console.WriteLine(shutdownMessage);
+                }
             }
         }
 
-        private static IConfiguration Configuration { get; } = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
-            .AddEnvironmentVariables()
-            .Build();
+        private static IConfiguration GetConfigurationBuilder(string environmentVariableKey)
+        {
+            if (string.IsNullOrWhiteSpace(environmentVariableKey))
+            {
+                throw new ArgumentException(nameof(environmentVariableKey));
+            }
+
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable(environmentVariableKey) ?? "Production"}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
 
         public static IWebHostBuilder CreateWebHostBuilder<T>(string[] args) where T : class =>
-            WebHost.CreateDefaultBuilder(args)
+            CreateWebHostBuilder<T>(new MainOptions { CommandLineArguments = args });
+
+        public static IWebHostBuilder CreateWebHostBuilder<T>(MainOptions options) where T : class =>
+            WebHost.CreateDefaultBuilder(options.CommandLineArguments)
                    .UseStartup<T>()
-                   .UseConfiguration(Configuration)
+                   .UseConfiguration(GetConfigurationBuilder(options.EnvironmentVariableKey))
                    .UseSerilog();
     }
 }
