ca994c203e7e79aea944092effdffed68d0def57
Pure Krome <pure.krome@world-domination.com.au>
2020-07-10 20:35:11 +1000

    :lipstick:

--- src/SampleWebApplication/Program.cs
+++ src/SampleWebApplication/Program.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Threading.Tasks;
 using Homely.AspNetCore.Hosting.CoreApp;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
